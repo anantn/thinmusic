531480249d7582def5f3047d04abee0f810a915c
Anant Narayanan <anant@kix.in>
2018-11-25 00:11:15 -0800

    Time on scrubber

--- src/Player.js
+++ src/Player.js
@@ -4,7 +4,8 @@ import {
   Button,
   Card,
   Elevation,
-  Slider
+  Slider,
+  Tooltip
 } from "@blueprintjs/core";
 
 import "./player.css";
@@ -24,36 +25,86 @@ const PS = window.MusicKit.PlaybackStates;
 class Player extends Component {
   constructor(props) {
     super(props);
+    this.interval = 0;
     this.state = {
-      playbackState: null
+      playbackState: null,
+      sliderHover: false
     };
+    this.slider = React.createRef();
   }
 
   componentDidMount = () => {
     let self = this;
-    this.interval = setInterval(this.tick, 300);
     this.props.music.addEventListener("playbackStateDidChange", event => {
       self.setState({ playbackState: event.state });
-      console.log(event);
+      switch (event.state) {
+        case PS.playing:
+          this.interval = setInterval(this.tick, 300);
+          break;
+        default:
+          if (this.interval !== 0) {
+            clearInterval(this.interval);
+            this.interval = 0;
+          }
+      }
     });
   };
 
   componentWillUnmount = () => {
-    clearInterval(this.interval);
+    if (this.interval !== 0) {
+      clearInterval(this.interval);
+    }
     this.props.music.removeEventListener("playbackStateDidChange");
   };
 
   tick = () => {
     if (this.state.playbackState !== PS.playing) return;
     this.setState({
-      current: this.props.music.player.currentPlaybackTime,
-      total: this.props.music.player.currentPlaybackDuration
+      currentTime: this.props.music.player.currentPlaybackTime,
+      totalTime: this.props.music.player.currentPlaybackDuration
     });
   };
 
   tickLabel = num => {
-    if (typeof num !== "number" || num === 0) return "-";
+    if (typeof num !== "number") return "-";
     return Math.floor(num / 60) + ":" + (num % 60 < 10 ? "0" : "") + (num % 60);
+  };
+
+  hoverTime = event => {
+    var rect = this.slider.current.getBoundingClientRect();
+    return Math.floor(
+      ((event.clientX - rect.left) / (rect.right - rect.left)) *
+        this.props.music.player.currentPlaybackDuration
+    );
+  };
+
+  sliderHoverEnter = event => {
+    this.setState({
+      sliderHover: true,
+      hoverTime: this.hoverTime(event)
+    });
+  };
+
+  sliderHoverMove = event => {
+    this.setState({
+      hoverTime: this.hoverTime(event)
+    });
+  };
+
+  sliderHoverLeave = event => {
+    this.setState({ sliderHover: false });
+  };
+
+  sliderChange = num => {
+    this.props.music.player.pause();
+    this.setState({ currentTime: num });
+  };
+
+  sliderRelease = num => {
+    let self = this;
+    this.props.music.player.seekToTime(num).then(() => {
+      self.props.music.player.play();
+    });
   };
 
   toggle = () => {
@@ -76,33 +127,42 @@ class Player extends Component {
       : "";
 
     let currentState = this.state.playbackState;
-    if (
-      currentState === PS.loading ||
-      currentState === PS.playing ||
-      currentState === PS.paused ||
-      currentState === PS.waiting
-    ) {
+    if (currentState >= PS.loading && currentState <= PS.waiting) {
       let button = "play";
       if (currentState === PS.playing) {
         button = "pause";
       }
       content = (
         <div>
-          {
-            <div className="duration">
-              <span className="start">
-                {this.tickLabel(this.state.current)}
-              </span>
-              <span className="end">{this.tickLabel(this.state.total)}</span>
-            </div>
-          }
-          <Slider
-            min={0}
-            max={this.state.total}
-            labelRenderer={() => ""}
-            labelStepSize={this.state.total || Infinity}
-            value={this.state.current}
-          />
+          <div className="duration">
+            <span className="start">
+              {this.tickLabel(this.state.currentTime)}
+            </span>
+            <span className="end">{this.tickLabel(this.state.totalTime)}</span>
+          </div>
+          <div
+            ref={this.slider}
+            onPointerEnter={this.sliderHoverEnter}
+            onPointerLeave={this.sliderHoverLeave}
+            onPointerMove={this.sliderHoverMove}
+          >
+            <Tooltip
+              disabled={!this.state.sliderHover}
+              position={"top"}
+              content={this.tickLabel(this.state.hoverTime)}
+              wrapperTagName={"div"}
+            >
+              <Slider
+                min={0}
+                max={this.state.totalTime}
+                onChange={this.sliderChange}
+                onRelease={this.sliderRelease}
+                labelRenderer={() => ""}
+                labelStepSize={this.state.totalTime || Infinity}
+                value={this.state.currentTime}
+              />
+            </Tooltip>
+          </div>
           <h4>{title}</h4>
           <h5>{subtitle}</h5>
           <ButtonGroup large={true}>
